44806c926a444b819aba1fc4b0ac29d50d435637
RahulGangwal29 <rahulgangwal29@gmail.com>
2021-03-08 20:46:51 +0530

    latestrg

--- android/app/src/androidTest/java/com/example/myapp/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/myapp/MainActivityTest.java
@@ -4,10 +4,9 @@ import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-import com.example.myapp.MainActivityTest;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
-    @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
 }
