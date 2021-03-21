0a8c3f756fae937a1e83d5b6491ddcad60edde42
RahulGangwal29 <rahulgangwal29@gmail.com>
2021-03-21 13:06:22 +0530

    new changes

---
+++ ios/flutter_application_rgTests/flutter_application_rgTests.swift
@@ -0,0 +1,32 @@
+//
+//  flutter_application_rgTests.swift
+//  flutter_application_rgTests
+//
+//  Created by Rahul Gangwal on 30/12/1942 Saka.
+//
+
+import XCTest
+
+class flutter_application_rgTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
