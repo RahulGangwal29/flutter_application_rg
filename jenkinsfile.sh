7b3ed218e714ec8af175f466b4f0fa6a33e80d2b
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-22 18:24:21 +0530

    latest4

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,22 +1,31 @@
 
-cd $FCI_BUILD_DIR
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                cd $FCI_BUILD_DIR
 
 
-pushd android
+sh 'pushd android'
 
-./gradlew app:assembleAndroidTest
+sh './gradlew app:assembleAndroidTest'
 
-./gradlew app:assembleDebug -Ptarget="$FCI_BUILD_DIR/integration_test/app_test.dart"
+sh './gradlew app:assembleDebug -Ptarget="$FCI_BUILD_DIR/integration_test/app_test.dart"'
 
-popd
+sh 'popd'
 
-echo $GCLOUD_KEY_FILE | base64 --decode > ./rahulgangwal1992-7ed6a8713950.json
+sh 'echo $GCLOUD_KEY_FILE | base64 --decode > ./rahulgangwal1992-7ed6a8713950.json'
 
-gcloud auth activate-service-account --key-file=/Users/rahgangw/Downloads/rahulgangwal1992-7ed6a8713950.json
+sh 'gcloud auth activate-service-account --key-file=/Users/rahgangw/Downloads/rahulgangwal1992-7ed6a8713950.json'
 
-gcloud --quiet config set project rahulgangwal1992
+sh 'gcloud --quiet config set project rahulgangwal1992'
 
-gcloud firebase test android run \
-  --type instrumentation \
-  --app build/app/outputs/apk/debug/app-debug.apk \
-  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+sh 'gcloud firebase test android run --type instrumentation --app build/app/outputs/apk/debug/app-debug.apk --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk'
+
+            }
+        }
+    }
+}
