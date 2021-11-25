0a24d85879cacbcdbdf27c124da1ba013b68d07f
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-25 20:37:19 +0530

    latest16

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,21 +1,16 @@
-pushd android
 
-flutter build apk 
 
-./gradlew app:assembleAndroidTest
+pipeline {
+    agent any
 
-./gradlew app:assembleDebug -Ptarget=integration_test/app_test.dart
+environment {
+            flutter = "/Users/rahgangw/flutter/flutter/bin/flutter"
+        }
 
-popd
+    stages {
+        stage 'Assemble Android Test'
+        sh "./gradlew assembleDebug"
+        sh "./gradlew assembleDebugAndroidTest"
 
-echo $GCLOUD_KEY_FILE | base64 --decode > ./gcloud_key_file.json
-
-gcloud auth activate-service-account --key-file=gcloud_key_file.json
-
-gcloud --quiet config set project your-project-id
-
-# Run the Flutter integration tests as if they were regular Android instrumentation tests
-gcloud firebase test android run \
-  --type instrumentation \
-  --app build/app/outputs/apk/debug/app-debug.apk \
-  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+    }
+}
