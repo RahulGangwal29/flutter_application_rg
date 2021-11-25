31a82f00434f3f3afe8964e826abed4225ac56a9
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-25 20:48:18 +0530

    latest21

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -5,14 +5,16 @@ pipeline {
 
 environment {
             flutter = "/Users/rahgangw/flutter/flutter/bin/flutter"
+            buzz_test = "/Users/rahgangw/flutter_application_rg"
         }
 
     stages {
         stage ('Assemble Android Test')
         {
             steps{
-        ./gradlew assembleDebug
-        ./gradlew assembleDebugAndroidTest
+                sh 'cd $buzz_test/android'
+        sh './gradlew assembleDebug'
+        sh './gradlew assembleDebugAndroidTest'
             }
         }
 
