d0e25ae5fa9064cbd8becd61cb0e2e8ccdbef4fa
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-25 20:01:30 +0530

    latest13

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -5,19 +5,22 @@ pipeline {
 
 environment {
             flutter = "/Users/rahgangw/flutter/flutter/bin/flutter"
+            buzz_test = "/Users/rahgangw/flutter_application_rg"
         }
 
     stages {
         stage('Hello') {
             steps {
 
-sh "pushd android"              
+                sh 'cd $buzz_test'
 
-sh "gradlew :app:assembleDebug"
+sh 'pushd android'              
 
-sh "gradlew :app:assembleDebugAndroidTest"
+sh './gradlew :app:assembleDebug'
 
-sh "popd"
+sh './gradlew :app:assembleDebugAndroidTest'
+
+sh 'popd'
             }
         }
     }
