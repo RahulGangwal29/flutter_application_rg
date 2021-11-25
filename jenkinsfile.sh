06abeebd11787ad77f90177b0ec52bb1f32fd2a1
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-25 18:58:21 +0530

    latest10

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -19,14 +19,6 @@ sh './gradlew :app:assembleDebugAndroidTest'
 
 sh 'popd'
 
-echo $GCLOUD_KEY_FILE | base64 --decode > ./rahulgangwal1992-7ed6a8713950.json
-
-gcloud auth activate-service-account --key-file=/Users/rahgangw/Downloads/rahulgangwal1992-7ed6a8713950.json
-
-gcloud --quiet config set project rahulgangwal1992
-
-gcloud firebase test android run --type instrumentation --app build/app/outputs/apk/debug/app-debug.apk --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk
-
             }
         }
     }
