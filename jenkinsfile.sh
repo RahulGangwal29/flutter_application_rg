f130032623ff7fa2f9e5d8f844edbb2014ced5c5
rahul.gangwal <rahul.gangwal@publicissapient.com>
2021-11-25 20:45:41 +0530

    latest20

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -11,8 +11,8 @@ environment {
         stage ('Assemble Android Test')
         {
             steps{
-        sh '/Users/rahgangw/flutter_application_rg/android/gradlew assembleDebug'
-        sh '/Users/rahgangw/flutter_application_rg/android/gradlew assembleDebugAndroidTest'
+        ./gradlew assembleDebug
+        ./gradlew assembleDebugAndroidTest
             }
         }
 
